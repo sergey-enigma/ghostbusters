c602ec297267372bdcea914d7df39684402bcab3
sergey-enigma <63449539+sergey-enigma@users.noreply.github.com>
2020-06-20 13:24:42 +0300

    Site

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 $(document).ready(function () {
     const mobileMenuButton = $('.mobile-menu-button');
     const mobileMenu = $('.mobile-menu');
+    const tabs = $('.tab');
+
     mobileMenuButton.on('click', function() {
         mobileMenu.toggleClass('active');
         $('body').toggleClass('no-scroll');
+    });
+
+    tabs.on('click', function() {
+        tabs.removeClass('active');
+        $(this).toggleClass('active');
+
+        const activeTabContent = $(this).attr('data-target');
+        $('.tab-content').removeClass('visible');
+        $(activeTabContent).toggleClass('visible');
+    });
+
+    new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 25,
+        loop: true,
+        breakpoints: {
+            992: {
+                slidesPerView: 4
+            },
+            768: {
+                slidesPerView: 2
+            },
+            320: {
+                slidesPerView: 1,
+                navigation: {
+                    nextEl: '.button-next'
+                }
+            }
+        }
     });
 });
